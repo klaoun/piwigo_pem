8f3af270d1f161d632664839a3e1840a462e9b49
HWFord <lilypickle@hotmail.fr>
2022-04-08 10:36:26 +0200

    Add main file for plugin init

---
+++ main.inc.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: piwigo_pem
+Version: auto
+Description: Piwigo extension manager
+Plugin URI: 
+Author: HWFord
+Author URI: https://github.com/HWFord
+Has Settings: false
+*/
+
+defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
+
+if (basename(dirname(__FILE__)) != 'piwigo_pem')
+{
+  add_event_handler('init', 'piwigo_pem_error');
+  function piwigo_pem_error()
+  {
+    global $page;
+    $page['errors'][] = 'Piwigo extension manager folder name is incorrect, uninstall the plugin and rename it to "piwigo_pem"';
+  }
+  return;
+}
+
+// +-----------------------------------------------------------------------+
+// | Define plugin constants                                               |
+// +-----------------------------------------------------------------------+
+
+define('PEM_ID', basename(dirname(__FILE__)));
+define('PEM_PATH' , PHPWG_PLUGINS_PATH . PEM_ID . '/');
+define('PEM_DIR', PHPWG_ROOT_PATH . PWG_LOCAL_DIR . 'piwigo_pem/');
+
+// +-----------------------------------------------------------------------+
+// | Add event handlers                                                    |
+// +-----------------------------------------------------------------------+
+
+/**
+ * plugin initialization
+ */
+add_event_handler('init', 'pem_init');
+function pem_init()
+{
+}
