b26b792d7b32a41c0482a562ce787d245799aac3
HWFord <lilypickle@hotmail.fr>
2022-11-24 15:18:08 +0100

    add ws method to get number of downloads for each extension per category

--- include/ws_functions.inc.php
+++ include/ws_functions.inc.php
@@ -38,7 +38,14 @@ function pem_ws_add_methods($arr)
     array(
       'extension_type' => array('info'=>'Language, Theme, Tool, Plugin'),
     ),
-    'Get number of extensions depending on type.'
+  $service->addMethod(
+    'pem.extensions.getNumberDownloads',
+    'ws_pem_get_number_downloads',
+    array(
+      'extension_type' => array('info'=>'language, theme, tool, plugin'),
+    ),
+    'Get most downloded extension depending on type.'
+  );
   );
 
 }
@@ -161,7 +168,45 @@ function ws_pem_get_extension_highest_rated($params, &$service){
    WHERE categories.idx_category = 12
    ORDER BY rating_score DESC
    LIMIT 1
-  ;
-  ';
+
+/**
+ * Get number of downloads per category
+ */
+function ws_pem_get_number_downloads($params, &$service){
+  $category_id;
+  switch ($params['extension_type']) 
+  {
+    case 'language':
+      $category_id = 8;
+      break;
+    case 'theme':
+      $category_id = 10;
+      break;
+    case 'tool':
+      $category_id = 11;
+      break;
+    case 'plugin':
+      $category_id = 12;
+      break;
+  }
+
+  $query = '
+SELECT 
+    extensions.id_extension,
+    SUM(nb_downloads) AS download_count,
+    extensions.description
+  FROM piwigo_pem_pem_revisions AS revisions
+    LEFT JOIN piwigo_pem_pem_extensions AS extensions
+    ON revisions.idx_extension = extensions.id_extension
+      left JOIN piwigo_pem_pem_extensions_categories AS categories
+      ON extensions.id_extension = categories.idx_extension
+      WHERE categories.idx_category = '.$category_id.'
+  GROUP BY revisions.idx_extension
+  ORDER BY download_count DESC;
+;';
+
+  $number_downloads = query2array($query);
+
+  return $number_downloads;
 }
 
