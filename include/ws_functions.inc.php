19545112b608484e0f20481b4e667137dbe4ad07
HWFord <54360213+HWFord@users.noreply.github.com>
2023-01-11 13:51:12 +0100

    Update ws_functions.inc.php

--- include/ws_functions.inc.php
+++ include/ws_functions.inc.php
@@ -6,26 +6,35 @@ include_once(PEM_PATH . 'constants.inc.php');
 function pem_ws_add_methods($arr)
 {
   $service = &$arr[0];
+ 
+  $service->addMethod(
+    'pem.categories.getList',
+    'ws_pem_categories_get_list',
+    array(
+    ),
+    'Get list of categories.'
+  );
+
+  $service->addMethod(
+    'pem.categories.getInfo',
+    'ws_pem_categories_get_info',
+    array(
+      'category_id' => array('type'=>WS_TYPE_INT|WS_TYPE_POSITIVE),
+    ),
+    'Getcategory info.'
+  );
 
   $service->addMethod(
     'pem.extensions.getList',
-    'ws_pem_extensions_getList',
-    array(),
+    'ws_pem_extensions_get_list',
+    array(
+    ),
     'Get list of extensions.'
   );
 
   $service->addMethod(
-    'pem.extensions.getInfo',
-    'ws_pem_get_info',
-    array(
-      'extension_id' => array('type'=>WS_TYPE_INT|WS_TYPE_POSITIVE),
-    ),
-    'Get extension info.'
-  );
-
-  $service->addMethod(
     'pem.extensions.getCount',
-    'ws_pem_get_count',
+    'ws_pem_extensions_get_count',
     array(
       'extension_type' => array('info'=>'language, theme, tool, plugin'),
     ),
@@ -33,8 +42,17 @@ function pem_ws_add_methods($arr)
   );
 
   $service->addMethod(
+    'pem.extensions.getInfo',
+    'ws_pem_extensions_get_info',
+    array(
+      'extension_id' => array('type'=>WS_TYPE_INT|WS_TYPE_POSITIVE),
+    ),
+    'Get extension info.'
+  );
+
+  $service->addMethod(
     'pem.extensions.getHighestRated',
-    'ws_pem_get_highest_rated',
+    'ws_pem_extensions_get_highest_rated',
     array(
       'extension_type' => array('info'=>'language, theme, tool, plugin'),
     ),
@@ -42,8 +60,8 @@ function pem_ws_add_methods($arr)
   );
 
   $service->addMethod(
-    'pem.extensions.getNumberDownloads',
-    'ws_pem_get_number_downloads',
+    'pem.extensions.getMostDownloaded',
+    'ws_pem_extensions_get_most_downloaded',
     array(
       'extension_type' => array('info'=>'language, theme, tool, plugin'),
     ),
@@ -51,20 +69,20 @@ function pem_ws_add_methods($arr)
   );
 
   $service->addMethod(
-    'pem.extensions.getMostDownloaded',
-    'ws_pem_get_most_downloaded',
+    'pem.extensions.getMostRecent',
+    'ws_pem_extensions_get_most_recent',
     array(
       'extension_type' => array('info'=>'language, theme, tool, plugin'),
     ),
-    'Get most downloded extension depending on type.'
+    'Get most recent extension depending on type.'
   );
 
 }
 
 /**
- * Get list of extensions
+ * Get list of all extensions, no matter the category
  */
-function ws_pem_extensions_getList($params, &$service)
+function ws_pem_extensions_get_list($params, &$service)
 {
   $extension_infos_of = array();
 
@@ -75,8 +93,8 @@ SELECT
     username
   FROM '.PEM_EXT_TABLE.' AS e
     JOIN '.PEM_USER_TABLE.' AS u ON u.id_user = e.idx_user
-;
-';
+;';
+
   $extension_infos_of = query2array($query, 'id_extension');
 
   if (!isset($_REQUEST['format']))
@@ -91,8 +109,9 @@ SELECT
 
 /**
  * Get extension info
+ * params extension id
  */
-function ws_pem_get_info($params, &$service)
+function ws_pem_extensions_get_info($params, &$service)
 {
 
   if (!isset($params['extension_id']))
@@ -111,6 +130,7 @@ function ws_pem_get_info($params, &$service)
 SELECT
     id_extension,
     name,
+    description,
     username
   FROM '.PEM_EXT_TABLE.' AS e
     JOIN '.PEM_USER_TABLE.' AS u ON u.id_user = e.idx_user
@@ -132,7 +152,42 @@ SELECT
 /**
  * Get number of extensions depending on category
  */
-function ws_pem_get_count($params, &$service)
+function ws_pem_extensions_get_count($params, &$service)
+{
+  $category_id;
+  switch ($params['extension_type']) 
+  {
+    case 'language':
+      $category_id = 8;
+      break;
+    case 'theme':
+      $category_id = 10;
+      break;
+    case 'tool':
+      $category_id = 11;
+      break;
+    case 'plugin':
+      $category_id = 12;
+      break;
+  }
+  $query = '
+SELECT 
+  COUNT(id_extension)
+  FROM '.PEM_EXT_TABLE.' AS extensions
+    LEFT JOIN '.PEM_EXT_CAT_TABLE.' AS categories
+      ON extensions.id_extension = categories.idx_extension
+      WHERE categories.idx_category = '.$category_id.'
+;';
+  $count_of_extensions = pwg_db_fetch_row(pwg_query($query));
+  return $count_of_extensions;
+}
+
+
+/**
+ * Get highest rated extension depending on category used in params
+ * params category id
+ */
+function ws_pem_extensions_get_highest_rated($params, &$service)
 {
   $category_id;
   switch ($params['extension_type']) 
@@ -153,103 +208,38 @@ function ws_pem_get_count($params, &$service)
 
   $query = '
 SELECT 
-	  COUNT(id_extension)
-  FROM piwigo_pem_pem_extensions AS extensions
-    LEFT JOIN piwigo_pem_pem_extensions_categories AS categories
- 	    ON extensions.id_extension = categories.idx_extension
- 	    WHERE categories.idx_category = '.$category_id.'
-;
-';
-  $count_of_extensions = pwg_db_fetch_row(pwg_query($query));
-
-  return $count_of_extensions;
-}
-
-/**
- * Get highest rated extension depending on category
- */
-function ws_pem_get_highest_rated($params, &$service){
-  $category_id;
-  switch ($params['extension_type']) 
-  {
-    case 'language':
-      $category_id = 8;
-      break;
-    case 'theme':
-      $category_id = 10;
-      break;
-    case 'tool':
-      $category_id = 11;
-      break;
-    case 'plugin':
-      $category_id = 12;
-      break;
-  }
-
-  $query = '
-SELECT 
     id_extension,
-    "name",
+    name,
     description,
     rating_score
-  FROM piwigo_pem_pem_extensions AS extensions
-  left JOIN piwigo_pem_pem_extensions_categories AS categories
+  FROM '.PEM_EXT_TABLE.' AS extensions
+  left JOIN '.PEM_EXT_CAT_TABLE.' AS categories
    ON extensions.id_extension = categories.idx_extension
    WHERE categories.idx_category = '.$category_id.'
    ORDER BY rating_score DESC
    LIMIT 1
-  ;';
+;';
 
   $highest_rated = query2array($query);
+
+  if (!isset($_REQUEST['format']))
+  {
+    //Echo to be compatible with previous version of Piwigo
+    echo serialize($highest_rated);
+    exit;
+  }
+
+
+  $highest_rated = $highest_rated [0];
 
   return $highest_rated;
 }
 
 /**
- * Get number of downloads per category
+ * Get most downloaded for a type of extension defined in params
  */
-function ws_pem_get_number_downloads($params, &$service){
-  $category_id;
-  switch ($params['extension_type']) 
-  {
-    case 'language':
-      $category_id = 8;
-      break;
-    case 'theme':
-      $category_id = 10;
-      break;
-    case 'tool':
-      $category_id = 11;
-      break;
-    case 'plugin':
-      $category_id = 12;
-      break;
-  }
-
-  $query = '
-SELECT 
-    extensions.id_extension,
-    SUM(nb_downloads) AS download_count,
-    extensions.description
-  FROM piwigo_pem_pem_revisions AS revisions
-    LEFT JOIN piwigo_pem_pem_extensions AS extensions
-    ON revisions.idx_extension = extensions.id_extension
-      left JOIN piwigo_pem_pem_extensions_categories AS categories
-      ON extensions.id_extension = categories.idx_extension
-      WHERE categories.idx_category = '.$category_id.'
-  GROUP BY revisions.idx_extension
-  ORDER BY download_count DESC;
-;';
-
-  $number_downloads = query2array($query);
-
-  return $number_downloads;
-}
-
-/**
- * Get most downloaded depending on category
- */
-function ws_pem_get_most_downloaded($params, &$service){
+function ws_pem_extensions_get_most_downloaded($params, &$service)
+{
   $category_id;
   switch ($params['extension_type']) 
   {
@@ -270,6 +260,7 @@ function ws_pem_get_most_downloaded($params, &$service){
   $query = '
 SELECT
     extensions.id_extension,
+    name,
     SUM(nb_downloads) AS download_count,
     extensions.description
   FROM piwigo_pem_pem_revisions AS revisions
@@ -283,7 +274,137 @@ SELECT
   LIMIT 1
 ;';
 
-$most_downloaded = query2array($query);
+  $most_downloaded = query2array($query);
 
-return $most_downloaded;
+  if (!isset($_REQUEST['format']))
+  {
+    //Echo to be compatible with previous version of Piwigo
+    echo serialize($most_downloaded);
+    exit;
+  }
+
+  $most_downloaded= $most_downloaded[0];
+
+  return $most_downloaded;
+}
+
+/**
+ * Get most recent for a type of extension defined in params
+ */
+function ws_pem_extensions_get_most_recent($params, &$service)
+{
+  $category_id;
+  switch ($params['extension_type']) 
+  {
+    case 'language':
+      $category_id = 8;
+      break;
+    case 'theme':
+      $category_id = 10;
+      break;
+    case 'tool':
+      $category_id = 11;
+      break;
+    case 'plugin':
+      $category_id = 12;
+      break;
+  }
+
+  $query = '
+SELECT
+    extensions.id_extension,
+    name,
+    revisions.date,
+    extensions.description
+  FROM piwigo_pem_pem_revisions AS revisions
+    LEFT JOIN piwigo_pem_pem_extensions AS extensions
+    ON revisions.idx_extension = extensions.id_extension
+      LEFT JOIN piwigo_pem_pem_extensions_categories AS categories
+      ON extensions.id_extension = categories.idx_extension
+      WHERE categories.idx_category = '.$category_id.'
+  ORDER BY revisions.date DESC 
+  LIMIT 1
+;';
+
+  $most_recent= query2array($query);
+
+  if (!isset($_REQUEST['format']))
+  {
+    //Echo to be compatible with previous version of Piwigo
+    echo serialize($most_recent);
+    exit;
+  }
+
+  $most_recent= $most_recent[0];
+  $most_recent['formatted_date'] = format_date($most_recent['date']);
+  $most_recent['time_since']= time_since($most_recent['date'], $stop='weeks');
+
+  return $most_recent;
+}
+
+/**
+ * Get list of extension categories
+ */
+function ws_pem_categories_get_list()
+{
+  $categories_infos_of = array();
+
+  $query = '
+SELECT
+    id_category,
+    name,
+    description
+  FROM '.PEM_CAT_TABLE.'
+;';
+
+  $categories_infos_of = query2array($query, 'id_category');
+
+  if (!isset($_REQUEST['format']))
+  {
+    //Echo to be compatible with previous version of Piwigo
+    echo serialize($categories_infos_of);
+    exit;
+  }
+
+  return $categories_infos_of;
+}
+
+/**
+ * Get category info
+ * params extension id
+ */
+function ws_pem_categories_get_info($params, &$service)
+{
+
+  if (!isset($params['category_id']))
+  {
+    die('missing category id');
+  }
+
+  if (!preg_match('/^\d+$/', $params['category_id']))
+  {
+    die('expected format for category id');
+  }
+  
+  $category_infos_of = array();
+  
+  $query = '
+SELECT
+    id_category,
+    name,
+    description
+  FROM '.PEM_CAT_TABLE.' AS e
+  WHERE id_category = '.$params['category_id'].'
+;';
+
+  $category_infos_of = query2array($query, 'id_category');
+
+  if (!isset($_REQUEST['format']))
+  {
+    //Echo to be compatible with previous version of Piwigo
+    echo serialize($category_infos_of);
+    exit;
+  }
+
+  return $category_infos_of;  
 }
