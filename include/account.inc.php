11c30f5d4c217f012021f92d75b1730f82d432db
HWFord <54360213+HWFord@users.noreply.github.com>
2023-06-23 11:47:42 +0200

    Update account
    Start adding html, display user info and list of user extensions
    Smarty variables from previous version of PEM
    
    TODO
    adapt php to new needs, remove unused code

--- include/account.inc.php
+++ include/account.inc.php
@@ -8,15 +8,186 @@ if (isset($_GET['uid']))
     check_pwg_token();
   }
   
+  global $conf; 
+
   $current_extension_page_id = $_GET['uid'];
 
-  $template->set_filename('pem_page', realpath(PEM_PATH . 'template/account.tpl'));
+  // $template->set_filename('pem_page', realpath(PEM_PATH . 'template/account.tpl'));
 
   $template->assign(
     array(
     'PEM_PATH' => PEM_PATH,
     )
   );
+
+    
+  if (!isset($user['remind_every']))
+  {
+    $user = array_merge(
+      $user,
+      create_user_infos($user['id'], true)
+    );
+  }
+
+  // +-----------------------------------------------------------------------+
+  // | Form submission                                                       |
+  // +-----------------------------------------------------------------------+
+
+  if (isset($_POST['submit']))
+  {
+    if (!preg_match('/^(day|week|month)$/', $_POST['remind_every']))
+    {
+      die("hacking attempt!");
+    }
+
+    $query = '
+  UPDATE '.USER_INFOS_TABLE.'
+    SET remind_every = \''.$_POST['remind_every'].'\'
+    WHERE idx_user = '.$user['id'].'
+  ;';
+  pwg_query($query);
+
+    $user['remind_every'] = $_POST['remind_every'];
+
+    $page['infos'][] = 'parameters saved';
+  }
+
+  // +-----------------------------------------------------------------------+
+  // | Page display                                                          |
+  // +-----------------------------------------------------------------------+
+
+  $template->set_filename('pem_page', realpath(PEM_PATH . 'template/account.tpl'));
+  // $tpl->set_filenames(
+  //   array(
+  //     'page' => 'page.tpl',
+  //     'my' => 'my.tpl'
+  //   )
+  // );
+
+  $template->assign('remind_every', $user['remind_every']);
+
+  // Get owned extensions & other extensions
+
+  $extension_ids = get_extension_ids_for_user($user['id']);
+
+  // Get other extensions
+  // $query = '
+  // SELECT id_extension
+  //   FROM '.PEM_EXT_TABLE.' AS ext
+  //   INNER JOIN '.PEM_AUTHORS_TABLE.' AS aut
+  //     ON ext.id_extension = aut.idx_extension
+  //   WHERE aut.idx_user = \''.$user['id'].'\'
+  //   ORDER BY name ASC
+  // ;';
+  // $other_extension_ids = query2array($query, null, 'id_extension');
+
+  // echo('<pre>');print_r( $other_extension_ids);echo('</pre>');
+
+  // Gets the total information about the extensions
+  // $extension_ids = array_merge($other_extension_ids, $my_extension_ids);
+
+  // $extension_ids = $my_extension_ids;
+
+  if (count($extension_ids) > 0)
+  {
+    // This query is used to get the publish date of extension,
+    // By getting the date of first revision
+    $query = '
+SELECT
+      MIN(date) as date,
+      idx_extension
+  FROM '.PEM_REV_TABLE.'
+    WHERE idx_extension IN ('.implode(',',$extension_ids).')
+    GROUP BY idx_extension
+    ORDER BY date ASC
+;';
+    $result = pwg_query($query);
+    while ($row = pwg_db_fetch_array($result))
+    {
+      $publish_extension_date_of[ $row['idx_extension'] ] = format_date($row['date']);
+    }
+
+    //This query is used to get the date of the extensions last update, 
+    $query = '
+SELECT
+    MAX(date) as date,
+    idx_extension
+FROM '.PEM_REV_TABLE.'
+  WHERE idx_extension IN ('.implode(',',$extension_ids).')
+  GROUP BY idx_extension
+  ORDER BY date ASC
+;';
+
+    $result = pwg_query($query);
+
+    $revision_of = $revision_ids_of = array();
+    while ($row = pwg_db_fetch_array($result))
+    {
+      // $revision_of[ $row['idx_extension'] ] = $row['version'];
+      // $revision_ids_of[ $row['idx_extension'] ] = $row['id_revision'];
+      $last_revision_date_of[ $row['idx_extension'] ] = format_date($row['date']);
+    }
+
+    $extension_infos_of = get_extension_infos_of($extension_ids);
+    $download_of_extension = get_download_of_extension($extension_ids);
+    $category_of_extension = get_categories_of_extension($extension_ids);
+
+    $query = '
+SELECT 
+    COUNT(rate) AS total,
+    idx_extension
+  FROM '.PEM_RATE_TABLE.'
+  WHERE idx_extension IN ('.implode(',',$extension_ids).')
+  GROUP BY idx_extension
+;';
+    $total_rates_of_extension = query2array($query, 'idx_extension', 'total');
+
+    foreach ($extension_ids as $extension_id)
+    {
+      $extension = array(
+        'id' => $extension_id,
+        'name' => htmlspecialchars(strip_tags($extension_infos_of[$extension_id]['name'])),
+        'category' => $category_of_extension[$extension_id]['default_name'],
+        'rating_score' => generate_static_stars($extension_infos_of[$extension_id]['rating_score'],0),
+        'total_rates' =>  isset($total_rates_of_extension[$extension_id]) ? $total_rates_of_extension[$extension_id] : '',
+        'nb_reviews' => isset($extension_infos_of[$extension_id]['nb_reviews']) ? $extension_infos_of[$extension_id]['nb_reviews'] : '',
+        'nb_downloads' => isset($download_of_extension[$extension_id]) ? $download_of_extension[$extension_id] : '',
+        'last_updated'=> $last_revision_date_of[$extension_id],
+        'publish_date' => $publish_extension_date_of[$extension_id],
+      );
+
+
+      if (in_array($extension_id, $extension_ids))
+      {
+        $template->append('extensions', $extension);
+      }
+    }
+  }
+
+  $registration_date_formatted = format_date($user['registration_date']);
+  $member_since = time_since($user['registration_date'], $stop='month');
+
+  $current_user = array();
+
+  $current_user['username'] = $user['username']; 
+  $current_user['registration_date_formatted'] = $registration_date_formatted;
+  $current_user['member_since'] = $member_since;
+
+  // Assign user info to tpl
+  if(in_array($user['id'], $conf['admin_users'])){
+    $current_user['group'] = 'Piwigo team <img class="certification_blue" src="'.get_absolute_root_url() . PEM_PATH.'images/CertificationBlue.svg"/>';
+  }
+  else if (in_array($user['id'], $conf['translators']))
+  {
+    $current_user['group'] = 'Translator';
+  }
+
+  $current_user['nb_extensions'] = count($extension_ids);
+
+
+  $template->assign('USER' , $current_user);
+
+  // echo('<pre>');print_r($current_user);echo('</pre>');
 }
 else
 {
--- template/account.tpl
+++ template/account.tpl
@@ -1,6 +1,58 @@
+<link rel="stylesheet" type="text/css" href="{$PEM_ROOT_URL_PLUGINS}styles/css/account.css">
+
 <div class="container" id="profile">
-<div>
-<a href="{$PEM_ROOT_URL}?act=logout">Logout</a>
-</div>
+
+  <section class="section-fluid my-5">
+    <div class="row">
+      <div class="col-4 border-right">
+        <h3>{$USER.username}</h3>
+       {if isset($USER.group)}<h5>{$USER.group}</h5>{/if }
+      </div>
+{if isset($USER.links)}
+      <div class="col-4 border-right">
+      </div>
+{/if}
+      <div class="col-4">
+        <p class="sub-text">Member since:</p>
+        <p class="d-inline">{$USER.registration_date_formatted}</p><span class="badge blue-badge d-inline">{$USER.member_since}<span>
+      </div>
+    </div>
+  </section>
+{if isset($extensions) || isset($other_extensions)}
+  <section class="section-fluid my-5">
+    <div class="row">
+      <div class="col-12">
+        <h4 class="d-inline">Extensions by "{$USER.username}"</h4><span class="badge blue-badge d-inline">{$USER.nb_extensions}</span>
+      </div>
+      <div class="col-12 my-5">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Publish date</th>
+            <th>Last update</th>
+            <th>Downloads</th>
+            <th>Rating</th>
+          </tr>
+        <thead>
+        <tbody>
+  {foreach from=$extensions item=extension}
+          <tr>
+            <td><a href="{$PEM_ROOT_URL}index.php?eid={$extension.id}">{$extension.name}</a></td>
+            <td>{$extension.category}</td>
+            <td>{$extension.publish_date}</td>
+            <td>{$extension.last_updated}</td>
+            <td>{$extension.nb_downloads}</td>
+            <td>{if $extension.rating_score}{$extension.rating_score}({$extension.total_rates}){/if}</td>
+          </tr>
+  {/foreach}
+        </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
+{/if}
+
 
 </div>
