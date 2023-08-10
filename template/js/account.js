a9e85e04caa09498d9f910dfc4c27f41f876a928
HWFord <54360213+HWFord@users.noreply.github.com>
2023-08-10 17:03:11 +0200

    Update account.js
    selectize modal inputs

--- template/js/account.js
+++ template/js/account.js
@@ -1,3 +1,18 @@
 jQuery("#edit_mode").change(function() {
   jQuery('.edit_mode').toggle();
 });
+
+// Selectize modal inputs
+jQuery('.extension_author_select').selectize({
+  plugins: ["remove_button"],
+})
+
+jQuery('.extension_tag_select').selectize({
+  plugins: ["remove_button"],
+})
+
+jQuery('.extension_lang_desc_select').selectize({
+  plugins: ["remove_button"],
+})
+
+jQuery('.extension_category_select').selectize();
