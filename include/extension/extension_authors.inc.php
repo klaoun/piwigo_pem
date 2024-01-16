be19a2b1daa7a9666811e70baaa6803cc5182738
HWFord <54360213+HWFord@users.noreply.github.com>
2024-01-16 10:44:29 +0100

    Update extension_authors.inc.php
    Remove uneeded code, duplicate that is in single_view.inc

--- include/extension/extension_authors.inc.php
+++ include/extension/extension_authors.inc.php
@@ -5,32 +5,6 @@
 
 global $template;
 
-// We need a valid extension
-$page['extension_id'] =
-  (isset($_GET['eid']) and is_numeric($_GET['eid']))
-  ? $_GET['eid']
-  : '';
-
-if (empty($page['extension_id']))
-{
-  die('Incorrect extension identifier');
-}
-
-$extension_infos = get_extension_infos_of($page['extension_id']);
-
-$query = '
-SELECT name
-  FROM '.PEM_EXT_TABLE.'
-  WHERE id_extension = '.$page['extension_id'].'
-;';
-$result = pwg_query($query);
-
-if (!pwg_db_num_rows($result))
-{
-  die('Incorrect extension identifier');
-}
-list($page['extension_name']) = pwg_db_fetch_array($result);
-
 // +-----------------------------------------------------------------------+
 // |                           Form submission                             |
 // +-----------------------------------------------------------------------+
@@ -61,37 +35,14 @@ INSERT INTO '.PEM_AUTHORS_TABLE.' (idx_extension, idx_user)
 ;';
       pwg_query($query);
     }
+
+    $template->assign(
+      array(
+        'MESSAGE' => 'Extension authors succefully updated.',
+        'MESSAGE_TYPE' => 'success'
+      )
+    );
   }
 }
-
-// +-----------------------------------------------------------------------+
-// |                            Form display                               |
-// +-----------------------------------------------------------------------+
-
-// Get list of all users
-$query = '
-SELECT '.$conf['user_fields']['id'].' AS id,
-       '.$conf['user_fields']['username'].' AS username
-  FROM '.USERS_TABLE.'
-  ORDER BY username
-;';
-$result = pwg_query($query);
-
-$users = array(0 => '');
-while ($row = pwg_db_fetch_assoc($result))
-{
-  if (!empty($row['username']))
-  {
-    $users[$row['id']] = $row['username'];
-  }
-}
-
-$template->assign(
-  array(
-    'extension_name' => $page['extension_name'],
-    'u_extension' => 'extension_view.php?eid='.$page['extension_id'],
-    'users' => $users,
-    )
-  );
 
 ?>
