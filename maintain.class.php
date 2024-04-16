3308e408cba8ac98ab9f034288d1db680459bf22
HWFord <54360213+HWFord@users.noreply.github.com>
2024-04-16 14:48:44 +0200

    Update maintain.class.php
    remove creating folder this isn't what we need

--- maintain.class.php
+++ maintain.class.php
@@ -67,9 +67,6 @@ class piwigo_pem_maintain extends PluginMaintain
       throw new SmartyException("unable to write file {$PHPWG_ROOT_PATH}extension_view.php");
     }  
 
-    if (!is_dir(PHPWG_ROOT_PATH.'api')) {
-      mkdir(PHPWG_ROOT_PATH.'api', 0777, true);
-    }
   }
 
   /**
