70adf7c4d7ed100417f4e175a5e96e5ea768e295
HWFord <54360213+HWFord@users.noreply.github.com>
2024-05-17 14:40:28 +0200

    Update main.js
    fix  how spinner is handled, make sure all inputs are empty before displaying, or else it would not submit

--- template/js/main.js
+++ template/js/main.js
@@ -2,7 +2,17 @@
 function onSubmitDisplaySpinner(formId)
 {
   const inputs = jQuery('#'+formId+' [required]')
-  if(jQuery(inputs).val() !== ''){
+
+  console.log(inputs)
+
+  var allFieldsFilled = true
+  jQuery(inputs).each(function(i, input){
+    if(jQuery(input).val() == ''){
+      allFieldsFilled = false;
+    }
+  });
+  
+  if(false != allFieldsFilled){
     jQuery('#'+formId+' .btn').addClass('d-none');
     jQuery('#'+formId+' .spinner-border').removeClass('d-none');
   }
