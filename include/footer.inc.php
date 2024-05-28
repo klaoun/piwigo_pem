eb966f341f4a6923fc46e1865710078fd1de1565
HWFord <54360213+HWFord@users.noreply.github.com>
2024-05-28 10:32:40 +0200

    Update footer.inc.php
    if subdomain is found change cache path to store tpl in specific language file

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -13,18 +13,16 @@ $cache_path = $conf['data_location'].PEM_ID.'/porg_footer.cache.php';
 if (preg_match('#([a-z]{2,3})\.piwigo\.org#', $raw_url, $matches))
 {
   $subdomain = $matches[1];
-
+  $cache_path = $conf['data_location'].PEM_ID.'/'.$subdomain.'_porg_footer.cache.php';
 }
 
-  if (!is_file($cache_path) or filemtime($cache_path) < strtotime('1 hour ago'))
+if (!is_file($cache_path) or filemtime($cache_path) < strtotime('1 hour ago'))
+{
+  fetchRemote("https://".(isset($subdomain)?$subdomain.'.':'')."piwigo.org/ws.php?format=php&method=porg.footer.getTemplate", $result);
+  if (mkgetdir(dirname($cache_path)))
   {
-    fetchRemote("https://".(isset($subdomain)?$subdomain:'')."piwigo.org/ws.php?format=php&method=porg.footer.getTemplate", $result);
-
-    if (mkgetdir(dirname($cache_path)))
-    {
-      file_put_contents($cache_path, serialize($result));
-    }
+    file_put_contents($cache_path, serialize($result));
   }
-
+}
 
 echo(unserialize(file_get_contents($cache_path)));
